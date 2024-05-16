8a187aa74d6049881549eccad55b2d887601e59c
Saif <124804475+Nazmus-Saif@users.noreply.github.com>
2024-05-16 09:10:08 +0600

    Update msg.py

--- msg.py
+++ msg.py
@@ -2,17 +2,17 @@ from fbchat import Client
 from fbchat.models import *
 
 # Initialize the client (assuming you're already logged in on your device)
-client = Client("nazmus saif", "<password>")
+client = Client("CLIENT NAME", "<password>")
 
 # Find a specific user by their name
-search_name = "KZ Tanvir Zaman"
+search_name = "FACEBOOK PROFILE NAME"
 users = client.searchForUsers(search_name)
 if len(users) > 0:
     user = users[0]  # Get the first user (top result)
     friend_id = user.uid
 
     # Send a message to the specified user
-    message = "i am to obak"
+    message = "ENTER YOU MESSAGE"
     sent = client.send(Message(text=message), thread_id=friend_id, thread_type=ThreadType.USER)
     if sent:
         print("Message sent successfully!")
